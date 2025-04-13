ebb06be30284063800ce7e0626228f7d7120c09c
Teja Sri Pacharu <145421488+TejasriPacharu@users.noreply.github.com>
2025-04-13 15:49:47 +0530

    Update train_small.sh

--- train_small.sh
+++ train_small.sh
@@ -4,11 +4,15 @@ python training-script.py \
     --data_dir=./ \
     --batch_size=4 \
     --lr=0.001 \
-    --epochs=10\
+    --epochs=10 \
     --img_size=144 \
     --alpha=0.5 \
     --beta=0.5 \
     --save_dir=./debug_checkpoints \
     --vis_dir=./debug_visualizations \
     --window_size=4 \
-    --embed_dim=24
+    --embed_dim=24 \
+    --num_heads 2 2 2 2 \
+    --depths 2 2 2 2 \
+    --pretrain \
+    --pretrain_epochs=3
